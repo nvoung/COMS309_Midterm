b74dfb1a75d0fc1c8d141893f8bfb8ff2e7ca311
nvoung <nathanvoung@gmail.com>
2024-10-20 20:43:15 -0500

    File Structure Changes

---
+++ Checkout/checkout.js
@@ -0,0 +1,44 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+  const purchaseButton = document.querySelector(".add-cart");
+const minusBtn = document.querySelector(".minus");
+const cartNumber = document.querySelector(".cart-number");
+const plusBtn = document.querySelector(".plus");
+let cartCount = 0;
+//advent listener
+purchaseButton.addEventListener("click", function(){
+
+    //localStorage.setItem("name", );
+    window.location.href = "../../Checkout/index.html";
+  // }
+  });
+minusBtn.addEventListener("click", function () {
+  if (cartCount >= 1) {
+    cartCount -= 1;
+    cartNumber.textContent = cartCount;
+  }
+});
+
+plusBtn.addEventListener("click", function () {
+  if (cartCount <= 9) {
+    cartCount += 1;
+    cartNumber.textContent = cartCount;
+  }
+});
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
